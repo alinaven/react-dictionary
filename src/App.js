4e7dbac4514f5b6211e559556ee5c9875376f079
alinaven <95190009+alinaven@users.noreply.github.com>
2021-12-20 13:25:51 +0100

    Set up app
    Install bootstrap

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a className="btn btn-primary" href="#">
+          Hello{" "}
+        </a>
+      </header>
+    </div>
+  );
+}
