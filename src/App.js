e74110cb2827c2b58309649c59c84324b89c9063
alinaven <95190009+alinaven@users.noreply.github.com>
2021-12-20 13:30:03 +0100

    Add title

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Dictionary </h1>
         <p>
           Hosted on{" "}
           <a
